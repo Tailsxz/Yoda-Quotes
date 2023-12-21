7540d11411d9a74d1aa552e8c8432de87e55431f
Tai Phan <ptai7722@gmail.com>
2023-12-21 16:54:22 -0600

    Set up EJS For the first time!

--- server.js
+++ server.js
@@ -4,9 +4,11 @@ const {MongoClient} = require('mongodb');
 const app = express();
 
 const username = encodeURIComponent('tailsawesome1337');
-const password = encodeURIComponent('tM68JrIZJj3qpyiP')
+const password = encodeURIComponent('tM68JrIZJj3qpyiP');
 
-let uriConnectionString = `mongodb+srv://${username}:${password}@cluster0.ltfnp9i.mongodb.net/?retryWrites=true&w=majority`
+let uriConnectionString = `mongodb+srv://${username}:${password}@cluster0.ltfnp9i.mongodb.net/?retryWrites=true&w=majority`;
+
+app.set('view engine', 'ejs');
 
 // MongoClient.connect(uriConnectionString, (err, client) => {
 //   if (err) return console.error(err);
@@ -17,6 +19,36 @@ let uriConnectionString = `mongodb+srv://${username}:${password}@cluster0.ltfnp9
 MongoClient.connect(uriConnectionString)
   .then(client => {
     console.log('Connected to Database');
+    const db = client.db('star-wars-quotes');
+    const quotesCollection = db.collection('quotes');
+
+    app.listen(process.env.PORT || 3000), function() {
+      console.log('listening on 3000');
+    };
+    
+    app.get('/', (req, res) => {
+      // res.send('Hello World')//send method of the response object, allows us to send back a HTTP response. In this case a string of Hello World
+      res.sendFile(__dirname + '/index.html')
+      //allows you to transfer files across the web, will automagically set the Content-Type header field on the response object.
+      //__dirname is the current directory we are in
+      console.log(__dirname);
+      db.collection('quotes')
+        .find()
+        .toArray()
+        .then(results => console.log(results))
+        .catch(error => console.error(error));
+    })
+    
+    app.post('/quotes', (req, res) => {
+      quotesCollection
+        .insertOne(req.body)
+        .then(result => {
+          res.redirect('/');
+          console.log(result);
+        })
+        .catch(error => console.error(error));
+    });
+    
   })
   .catch(error => console.error(error));
 
@@ -26,19 +58,3 @@ app.use(express.urlencoded())//Parse URL-encoded bodies
 // app.use(bodyParser.json()) No longer needed!
 //app.use(express.json()); //used to parse JSON bodies.
 
-app.listen(process.env.PORT || 3000), function() {
-  console.log('listening on 3000');
-};
-
-app.get('/', (req, res) => {
-  // res.send('Hello World')//send method of the response object, allows us to send back a HTTP response. In this case a string of Hello World
-  res.sendFile(__dirname + '/index.html')
-  //allows you to transfer files across the web, will automagically set the Content-Type header field on the response object.
-  //__dirname is the current directory we are in
-  console.log(__dirname);
-})
-
-app.post('/quotes', (req, res) => {
-  console.log('Helloooooooooooo!');
-  console.log(req.body);
-});
