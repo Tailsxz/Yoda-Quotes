dcb17d38dabd737729ef98b6cff1a58aa18955e5
Tai Phan <ptai7722@gmail.com>
2023-12-21 16:17:33 -0600

    First connection to a database lets go!

--- server.js
+++ server.js
@@ -1,6 +1,30 @@
+// const bodyParser = require('body-parser');
 const express = require('express');
-const bodyParser = require('body-parser');
+const {MongoClient} = require('mongodb');
 const app = express();
+
+const username = encodeURIComponent('tailsawesome1337');
+const password = encodeURIComponent('tM68JrIZJj3qpyiP')
+
+let uriConnectionString = `mongodb+srv://${username}:${password}@cluster0.ltfnp9i.mongodb.net/?retryWrites=true&w=majority`
+
+// MongoClient.connect(uriConnectionString, (err, client) => {
+//   if (err) return console.error(err);
+//   console.log('Connected to Database');
+// })
+
+//Supports promises so we can write our code like this!
+MongoClient.connect(uriConnectionString)
+  .then(client => {
+    console.log('Connected to Database');
+  })
+  .catch(error => console.error(error));
+
+// app.use(bodyParser.urlencoded({ extended: true }))
+//deprecated! Express has a builtin body parser now build to work exactly like the body-parser tooling
+app.use(express.urlencoded())//Parse URL-encoded bodies
+// app.use(bodyParser.json()) No longer needed!
+//app.use(express.json()); //used to parse JSON bodies.
 
 app.listen(process.env.PORT || 3000), function() {
   console.log('listening on 3000');
@@ -16,4 +40,5 @@ app.get('/', (req, res) => {
 
 app.post('/quotes', (req, res) => {
   console.log('Helloooooooooooo!');
+  console.log(req.body);
 });
