3636609d24c9e0f27c9fe1cc60ac136a69035046
Tai Phan <ptai7722@gmail.com>
2023-12-21 15:24:43 -0600

    Sent our first index.html file!

--- server.js
+++ server.js
@@ -4,3 +4,11 @@ const app = express();
 app.listen(process.env.PORT || 3000), function() {
   console.log('listening on 3000');
 };
+
+app.get('/', (req, res) => {
+  // res.send('Hello World')//send method of the response object, allows us to send back a HTTP response. In this case a string of Hello World
+  res.sendFile(__dirname + '/index.html')
+  //allows you to transfer files across the web, will automagically set the Content-Type header field on the response object.
+  //__dirname is the current directory we are in
+  console.log(__dirname);
+})
